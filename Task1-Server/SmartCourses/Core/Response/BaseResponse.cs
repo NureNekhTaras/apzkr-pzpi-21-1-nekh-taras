920d10881fbbe1dfac7b6f92381e5b950fdba235
NexT <taras.hex@gmail.com>
2024-06-15 15:24:04 +0300

    Server+WebApp

---
+++ Task1-Server/SmartCourses/Core/Response/BaseResponse.cs
@@ -0,0 +1,17 @@
+﻿using Core.Enums;
+using Core.Response.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Response
+{
+    public class BaseResponse<T> : IBaseResponse<T>
+    {
+        public string Info { get; set; }
+        public StatusCode StatusCode { get; set; }
+        public T Data { get; set; }
+    }
+}
---
+++ Task1-Server/SmartCourses/Core/Static/AuthHelper.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Static
+{
+    public class AuthHelper
+    {
+        public static ClaimsIdentity Authenticate(User user)
+        {
+            var coursesIds = new List<int>();
+            var coursesNames = new List<string>();
+
+            foreach (var course in user.Profile.Courses)
+            {
+                coursesIds.Add(course.Id);
+                coursesNames.Add(course.Name);
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim("Login", user.Login),
+                new Claim("Role", user.Profile.Role.ToString()),
+                new Claim("Id", user.Id.ToString()),
+                new Claim("CoursesIds", string.Join(',', coursesIds)),
+                new Claim("CoursesNames", string.Join(',', coursesNames))
+            };
+
+            return new ClaimsIdentity(claims, "ApplicationCookie",
+                ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+        }
+    }
+}
